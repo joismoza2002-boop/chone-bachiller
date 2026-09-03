151d58bb37e5cabc25295967893500ee3cd887cc
joismoza2002-boop <joismoza2002@gmail.com>
2026-09-03 08:04:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -162,9 +162,14 @@ cursor = conn.cursor()
 # ==========================================
 ADMIN_EMAILS = ["admin@chonebachiller.edu", "Joismoza2002@gmail.com"]
 
-authenticator = Authenticate(st.secrets)
-
-
+authenticator = Authenticate(
+    client_id=st.secrets["client_id"],
+    client_secret=st.secrets["client_secret"],
+    redirect_uri=st.secrets["redirect_uri"],
+    cookie_name=st.secrets["cookie_name"],
+    cookie_key=st.secrets["cookie_key"],
+    cookie_expiry_days=30
+)
 
 if "profile_complete" not in st.session_state: st.session_state.profile_complete = False
 if "current_view" not in st.session_state: st.session_state.current_view = "dashboard"
